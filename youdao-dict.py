4c0af300d5c3dd71e0ec4ba157bd4439e3d65ce2
keroro520 <keroroxx520@gmail.com>
2014-09-24 17:28:50 +0800

    First

---
+++ youdao-dict.py
@@ -0,0 +1,62 @@
+#!/bin/python2
+#-*- coding: utf-8 -*-
+'''
+Python2用有道给的API写的在线命令行词典
+'''
+import json
+import urllib2
+import re
+import sys
+
+
+def escape(s):
+    def _print(x):
+        x = x.group()
+        code, x = x[0:2], x[2:]
+        if code != '\\x':
+            return x
+        return ('%'+str(x)).upper()
+
+    s = repr(s)
+    return re.sub(r'\\x[0-9a-fA-F]{2}', _print, s)
+
+
+def get_url(s):
+    url = 'http://fanyi.youdao.com/openapi.do?keyfrom=tinxing&key=1312427901&type=data&doctype=json&version=1.1&q='
+    s = escape(s)
+    return url+s[1:-1]
+
+
+def output(data):
+    if 'translation' in data:
+        print('traslation:')
+        for i in data['translation']:
+            print('  '+i)
+        print
+    if 'basic' in data and 'explains' in data['basic']:
+        print('explains: ')
+        for i in data['basic']['explains']:
+            print('  '+i)
+        print
+
+
+def lookup(s):
+    data = urllib2.urlopen(get_url(s)).read()
+    if data:
+        data = json.loads(data)
+        output(data)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        for i, s in enumerate(sys.argv[1:]):
+            lookup(s)
+            if i != len(sys.argv)-1:
+                print('-------------------------')
+    else:
+        while True:
+            s = raw_input()
+            if s.strip():
+                lookup(s.strip())
+            if s == '':
+                break
